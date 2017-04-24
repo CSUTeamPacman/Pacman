29d966fe6ef8b668ccaf10d3f67c9dd7a73964bd
Damon Wingo <dlwingo@gmail.com>
2017-04-24 16:26:52 -0400

    Add files via upload

---
+++ Pacman.java
@@ -0,0 +1,34 @@
+package paccy;
+
+/**
+ * Created by damonwingo on 4/24/17.
+ */
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+
+    public Pacman() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setTitle("Pacman");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 420);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            Pacman ex = new Pacman();
+            ex.setVisible(true);
+        });
+    }
+}
